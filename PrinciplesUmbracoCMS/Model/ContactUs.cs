90ddb35aa3cb96c0f66aa9870ec0b7675624f3d8
pc <pc@LAPTOP-2457G5FT>
2024-01-07 01:11:45 +0300

    xc

---
+++ PrinciplesUmbracoCMS/Model/ContactUs.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Principles_UmbracoCMS.Model
+{
+    public class ContactUs
+    {
+        [MaxLength(100)]
+        public string FullName { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }  = string.Empty;
+
+        [MaxLength(300)]
+        public string Message { get; set; } = string.Empty;
+    }
+}
